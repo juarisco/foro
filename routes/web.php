1c8fdfab7f0a4fb0afc3504e657f17a2f4d26c84
juarisco <juarisco@outlook.com>
2018-12-09 11:00:45 -0500

    Complete social authentication in laravel

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{provider}/auth', 'SocialsController@auth')->name('social.auth');
+
+Route::get('/{provider}/redirect', 'SocialsController@auth_callback')->name('social.callback');
